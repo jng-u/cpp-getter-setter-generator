9b7b319fe0ae1694243d33a753e793604c735028
jngu <ljw69700@gmail.com>
2023-04-26 10:29:21 +0900

    v0.2

---
+++ README.md
@@ -0,0 +1,9 @@
+# Getter And Setter Generator For C++
+
+1. Select your variable
+2. `Ctrl`+`Shift`+`p`
+3. Select `Generate Getter`, `Generate Setter` or `Generate Getter And Setter`
+4. Generate getter/setter code to clipboard
+5. Paste code! 
+
+![example](sample/test.gif)
---
+++ extension.js
@@ -0,0 +1,122 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	console.log('Activate cpp getter and setter generator');
+	
+	const get_highlighted_text = function() {
+		const editor = vscode.window.activeTextEditor;
+        const selection = editor?.selection;
+        if (selection && !selection.isEmpty) {
+            const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
+            const highlighted = editor.document.getText(selectionRange);
+			return highlighted;
+		}
+		return '';
+	}
+
+    const gen_getter = vscode.commands.registerCommand('extension.generate_getter', () => {
+        // The code you place here will be executed every time your command is executed	
+        let highlighted = get_highlighted_text();
+        if (highlighted.length != 0) {
+            const lines = highlighted.split('\n');
+            let getter_string = '';
+            // check validate line			
+            for (let line of lines) {
+                line = line.trim();
+                if (line.length == 0)
+                    continue;
+                let split_index = line.indexOf(' ');
+                let vartype = line.substring(0, split_index);
+                let vars = line.substring(split_index + 1, line.length - 1).split(',');
+                for (let _var of vars) {
+                    _var = _var.trim();
+                    if (_var.endsWith(')'))
+                        _var = _var.substring(0, _var.indexOf('('));
+                    getter_string += 'inline ' + vartype + ' get_' + _var + '() const { return ' + _var + '; }\n';
+                }
+            }
+            vscode.env.clipboard.writeText(getter_string);
+            // Display a message box to the user
+            vscode.window.showInformationMessage('paste your setter code!');
+        }
+    });
+
+	const gen_setter = vscode.commands.registerCommand('extension.generate_setter', () => {
+        // The code you place here will be executed every time your command is executed	
+        let highlighted = get_highlighted_text();
+        if (highlighted.length != 0) {
+            const lines = highlighted.split('\n');
+            let setter_string = '';
+            // check validate line			
+            for (let line of lines) {
+                line = line.trim();
+                if (line.length == 0)
+                    continue;
+                let split_index = line.indexOf(' ');
+                let vartype = line.substring(0, split_index);
+                let vars = line.substring(split_index + 1, line.length - 1).split(',');
+                for (let _var of vars) {
+                    _var = _var.trim();
+                    if (_var.endsWith(')'))
+                        _var = _var.substring(0, _var.indexOf('('));
+                    setter_string += 'void set_' + _var + '(const ' + vartype + ' &' + _var + ') {\n this->' + _var + ' = ' + _var + '; \n}\n';
+                }
+                // valid_lines.push(line);
+                // console.log(line);
+            }
+            vscode.env.clipboard.writeText(setter_string);
+            // Display a message box to the user
+            vscode.window.showInformationMessage('paste your getter code!');
+        }
+    });
+
+	const gen_getter_setter = vscode.commands.registerCommand('extension.generate_getter_setter', () => {
+        // The code you place here will be executed every time your command is executed	
+        let highlighted = get_highlighted_text();
+        if (highlighted.length != 0) {
+            const lines = highlighted.split('\n');
+            let getter_string = '';
+            let setter_string = '';
+            // check validate line			
+            for (let line of lines) {
+                line = line.trim();
+                if (line.length == 0)
+                    continue;
+                let split_index = line.indexOf(' ');
+                let vartype = line.substring(0, split_index);
+                let vars = line.substring(split_index + 1, line.length - 1).split(',');
+                for (let _var of vars) {
+                    _var = _var.trim();
+                    if (_var.endsWith(')'))
+                        _var = _var.substring(0, _var.indexOf('('));
+					getter_string += 'inline ' + vartype + ' get_' + _var + '() const { return ' + _var + '; }\n';
+                    setter_string += 'void set_' + _var + '(const ' + vartype + ' &' + _var + ') {\n	this->' + _var + ' = ' + _var + '; \n}\n';
+                }
+            }
+            vscode.env.clipboard.writeText(getter_string+'\n'+setter_string);
+            // Display a message box to the user
+            vscode.window.showInformationMessage('paste your getter and setter code!');
+        }
+    });
+
+	context.subscriptions.push(gen_getter);
+	context.subscriptions.push(gen_setter);
+	context.subscriptions.push(gen_getter_setter);
+}
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+// eslint-disable-next-line no-undef
+module.exports = {
+	activate,
+	deactivate
+}
